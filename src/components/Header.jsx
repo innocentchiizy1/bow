640f98e04ffacf248aaac7a82765061d952fdc46
innocentchiizy1 <innocentchiizy1@gmail.com>
2025-01-04 16:01:07 +0100

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/header.css";
+import "../styles/Header.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
